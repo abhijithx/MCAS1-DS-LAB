d29c141ab714f1faac106db9ce59f829acc5785a
abhijithx <abhijithsudhakaran123@gmail.com>
2025-08-30 14:19:54 +0530

    untracked exe files

--- simpleStack.c
+++ simpleStack.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 
+// echo "# MCAS1-DS-LAB" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/abhijithx/MCAS1-DS-LAB.git
+// git push -u origin main
+
+
+// or push an existing repository from the command line
+// git remote add origin https://github.com/abhijithx/MCAS1-DS-LAB.git
+// git branch -M main
+// git push -u origin main
+
+
 int main(){
-    printf("hello world  abhijith");
+    printf("hello world");
     return 0;
 }
