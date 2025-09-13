b6bb72cd4671d5d4498a44184fea2f60bed5be94
abhijithx <abhijithsudhakaran123@gmail.com>
2025-09-14 02:27:15 +0530

    simple Linked list .c modified

--- simpleLinkedList.c
+++ simpleLinkedList.c
@@ -1,5 +1,48 @@
-#include<stdio.h>
-int main(){
-    printf("hello world");
+#include <stdio.h>
+#include <stdlib.h>
+
+int size = 0;
+
+// Complete struct definition
+struct node {
+    int data;
+    struct node* next;
+};
+
+// global head and tail
+struct node* head = NULL;
+struct node* tail = NULL;
+
+// Insert at beginning
+void insertAtFirst(int val) {
+    struct node* newNode = malloc(sizeof(struct node)); // ✅ works
+    newNode->data = val;
+    newNode->next = head;
+    head = newNode;
+
+    if (tail == NULL) {
+        tail = head;
+    }
+
+    size++;
+}
+
+// Print list
+void printList() {
+    struct node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    insertAtFirst(10);
+    insertAtFirst(20);
+    insertAtFirst(30);
+
+    printList();
+    printf("Size = %d\n", size);
     return 0;
 }
